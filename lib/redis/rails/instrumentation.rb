81c29068a148aefcd324e979a512d47b44cef0f2
ryantaylor <ryan.taylor@overbond.com>
2017-03-22 12:23:54 -0400

    Replace binary data with placeholder text when logging.

--- lib/redis/rails/instrumentation.rb
+++ lib/redis/rails/instrumentation.rb
@@ -17,6 +17,7 @@ class Redis
 
           output = cmds.map do |name, *args|
             if !args.empty?
+	      args = args.map { |arg| arg.respond_to?(:encoding) && arg.encoding == Encoding::ASCII_8BIT ? 'BINARY DATA' : arg }
               "[ #{name.to_s.upcase} #{args.join(' ')} ]"
             else
               "[ #{name.to_s.upcase} ]"
